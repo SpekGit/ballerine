b3e5d0a48a3632d847a2da1c17ae34179b7585ec
Haller125 <connect@spektal.com>
2024-03-27 15:29:38 +0500

    turn js into ts

--- services/workflows-service/plugins/verify-repository-project-scoped/verify-repository-project-scoped.rule.ts
+++ services/workflows-service/plugins/verify-repository-project-scoped/verify-repository-project-scoped.rule.ts
@@ -1,14 +1,15 @@
-module.exports = {
+import { Rule } from 'eslint';
+
+const rule: Rule.RuleModule = {
   meta: {
     type: 'problem',
-    severiry: 'error',
     docs: {
       description: 'Check if repository methods recieve projectId: TProjectId',
       category: 'Best Practices',
       recommended: true,
     },
   },
-  create: context => {
+  create: (context: Rule.RuleContext) => {
     const isRepository =
       /^(?!.*\/(customer|data-migration)\.repository\.(ts|js)$).*\.repository\.(ts|js)$/.test(
         // Using deprecated .getFilename here because relevant context.filename returns undefined
@@ -18,17 +19,17 @@ module.exports = {
     const UNSCOPED_METHOD_NAMES = ['unscoped', 'create', 'update'];
 
     return {
-      MethodDefinition: node => {
+      MethodDefinition: (node: any) => {
         if (!isRepository || node.key.name === 'constructor') return;
 
-        const isUnscoped = UNSCOPED_METHOD_NAMES.some(name =>
+        const isUnscoped = UNSCOPED_METHOD_NAMES.some((name: string) =>
           node.key.name.toLowerCase().includes(name),
         );
 
         if (isUnscoped) return;
 
         const isProjectIdsIncluded = node.value.params.some(
-          param => param.type === 'Identifier' && param.name.toLowerCase().includes('projectid'),
+          (param: any) => param.type === 'Identifier' && param.name.toLowerCase().includes('projectid'),
         );
 
         if (!isProjectIdsIncluded) {
@@ -41,3 +42,5 @@ module.exports = {
     };
   },
 };
+
+export default rule;
